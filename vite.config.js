3ff285b25163574e57b7d25ac0132cd382be3398
Arnol-0 <arnol-0@users.noreply.github.com>
2026-08-28 10:36:48 -0400

    Add legacy plugin for Smart TV compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'Chrome >= 49', 'Safari >= 10', 'iOS >= 10', 'Firefox >= 50', 'Edge >= 14'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  server: {
+    host: true
+  }
 })
